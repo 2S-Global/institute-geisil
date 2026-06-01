2dda63f5ff354530c83dfca9daf79ecbb9ed7487
soumya <soumya@2sglobal.us>
2026-06-01 14:50:41 +0530

    update

--- src/components/EmployerSidebar.tsx
+++ src/components/EmployerSidebar.tsx
@@ -34,7 +34,7 @@ const main = [
   { title: "Interviews", url: "/employer/interviews", icon: CalendarCheck },
   { title: "Assessments", url: "/employer/assessments", icon: ClipboardList },
   { title: "Reports", url: "/employer/reports", icon: BarChart3 },
-  { title: "Post a New Job", url: "/employer/post-jobs", icon: BarChart3 },
+  { title: "Post a New Job", url: "/employer/post-jobs", icon: Briefcase },
   { title: "Verify Employee", url: "/employer/verify-employee", icon: UserCheck },
 ];
 
