6e6fbc8efc0d085d6de2484092466c4cbd1a1991
soumya@2sglobal.us <soumyasglobal>
2026-09-23 18:53:10 +0530

    update

--- src/pages/candidate/CandidateApplicationDetail.tsx
+++ src/pages/candidate/CandidateApplicationDetail.tsx
@@ -553,7 +553,7 @@ export default function CandidateApplicationDetail() {
           <div className="lg:col-span-2 space-y-6">
             <Tabs defaultValue="timeline">
               <TabsList className="w-full justify-start flex-wrap h-auto">
-                <TabsTrigger value="timeline">Timeline jjjjjjj</TabsTrigger>
+                <TabsTrigger value="timeline">Timeline mmm</TabsTrigger>
                 <TabsTrigger value="overview">Job overview</TabsTrigger>
                 {/* <TabsTrigger value="interviews">Interviews</TabsTrigger> */}
                 <TabsTrigger value="messages">Messages</TabsTrigger>
